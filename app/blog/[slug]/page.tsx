0e23172e556d6c885e0b8d29a9b92a7404fb6a1c
SdZika <markozivkovic1982@gmail.com>
2024-09-22 21:13:50 +0200

    portabletext working

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import { client } from "@/sanity/client";
 import { defineQuery } from "next-sanity";
 import { notFound } from "next/navigation";
-import {PortableText} from '@portabletext/react'
+import {PortableText, PortableTextComponents} from '@portabletext/react'
+import { urlFor } from "@/sanity/client";
 
 const EVENT_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
     
@@ -27,7 +28,38 @@ const EVENT_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
         body
         } = blog; 
 
-        
+        const components: PortableTextComponents = {
+          types: {
+            image: ({ value }) => (
+              <img src={urlFor(value).url()} alt={value.alt || 'Image'} />
+            ),
+            callToAction: ({ value, isInline }) =>
+              isInline ? (
+                <a href={value.url}>{value.text}</a>
+              ) : (
+                <div className="callToAction">{value.text}</div>
+              ),
+          },
+          marks: {
+            link: ({ children, value }) => {
+              const rel = !value.href.startsWith('/') ? 'noreferrer noopener' : undefined;
+              return (
+                <a href={value.href} rel={rel}>
+                  {children}
+                </a>
+              );
+            },
+          },
+          block: {
+            normal: ({ children }) => <p className="my-4 text-gray-700">{children}</p>,
+            h1: ({ children }) => <h1 className="text-3xl font-bold my-4">{children}</h1>,
+            h2: ({ children }) => <h2 className="text-2xl font-semibold my-3">{children}</h2>,
+            h3: ({ children }) => <h3 className="text-xl font-medium my-2">{children}</h3>,
+            blockquote: ({ children }) => (
+            <blockquote className="pl-4 border-l-4 border-gray-300 italic">{children}</blockquote>
+            ),
+          },
+        };
 
           
     return (
@@ -38,9 +70,7 @@ const EVENT_QUERY = defineQuery(`*[_type == "post" && slug.current == $slug][0]{
            
           {name ? <h1 className="primary-color text-4xl font-bold mb-4">{name}</h1> : null}
           {publishedAt ? <p className="text-gray-400 mb-2">{publishedAt}</p> : null}
-          {body && body.length > 0 && (<PortableText value={body} components={  {block: {
-    
-    h2: ({children}) => <h2 className="text-2xl text-white">{children}</h2>}}}/>)}
+          {body && body.length > 0 && (<PortableText value={body} components={components }/>)}
          
    
       </main>
