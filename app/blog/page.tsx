d81208909410f79e313b9b493eb6e85f578ecd8e
SdZika <markozivkovic1982@gmail.com>
2024-09-26 14:56:12 +0200

    add blog types

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -12,7 +12,7 @@ const EVENTS_QUERY = defineQuery(`*[_type == "post" && defined(slug.current)][0.
     name,
     publishedAt,
     slug,
-    
+    postType
   }`);
 
 export default async function IndexPage() {
@@ -28,14 +28,15 @@ export default async function IndexPage() {
       </div>
 
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-6">
-        {blogs.map((blog: {name: string, slug: {current: string}, publishedAt: string}) => (
+        {blogs.map((blog: {name: string, slug: {current: string}, publishedAt: string, postType: string, _id: string}) => (
           <li className="bg-[#161616] p-6 rounded-xl shadow-md transform transition-transform duration-300 hover:scale-105" key={blog._id}>
             <Link
               className="hover:underline"
               href={`/blog/${blog?.slug?.current}`}
             >
+              <p className="px-6 py-3 w-full rounded-xl mr-4 border border-gray-400 text-white">{blog?.postType}</p>
               <h2 className="text-2xl font-semibold primary-color">{blog?.name}</h2>
-              {console.log(`here is blog name ${blog.name}`)}
+              
             </Link>
           </li>
         ))}
