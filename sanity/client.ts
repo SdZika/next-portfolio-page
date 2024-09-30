7f948ea2ba7a7c49983335d8074c8f17f1e80e52
SdZika <markozivkovic1982@gmail.com>
2024-09-30 15:17:57 +0200

    protect env file

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,8 +1,9 @@
 import { createClient } from "next-sanity";
+import {dataset, projectId} from './env'
 
 export const client = createClient({
-  projectId: "wqxigzwn",
-  dataset: "production",
+  projectId,
+  dataset,
   apiVersion: "2024-01-01",
   useCdn: true,
 });
