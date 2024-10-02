0c896ac74061e3d9492b6b90bf7b33d7cc6156e7
SdZika <markozivkovic1982@gmail.com>
2024-10-02 22:02:35 +0200

    change logout component

--- app/logout/actions.ts
+++ app/logout/actions.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from 'next/cache'
 import { createClient } from '@/supabase/server'
-// import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export async function logout() {
     const supabase = createClient()
