2bb6ee8125be49ef511b9413eac4440253b164a9
SdZika <markozivkovic1982@gmail.com>
2024-10-03 15:28:46 +0200

    trying to handele login logout how to show

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import { HamburgerMenu } from './HamburgerMenu'
 import { logout } from '../logout/actions'
+import PrivatePage from '../private/page'
 
 export const Navbar = () => {
   return (
@@ -32,8 +33,9 @@ export const Navbar = () => {
                 </form>
                 
             </li>
-
+          
         </ul>
+        
         <HamburgerMenu />
     </nav>
     )
