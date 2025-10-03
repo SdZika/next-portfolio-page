c11fb6dcdbad36489af3249edbc739304dd91f14
SdZika <markozivkovic1982@gmail.com>
2025-10-03 15:13:12 +0200

    navbar refactor

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,26 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HamburgerMenu } from './HamburgerMenu'
 import { logout } from '../logout/actions'
-
+import { supabase } from '@/supabase/supabaseClient'
 
 export const Navbar = () => {
+
+
+    
+const  user = supabase?.auth.getUser()
+
+  console.log('user is here', user)
+
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(() => {
+
+    setLoggedIn(true)
+
+  }, [user])
+
   return (
     <nav className="border-b border-gray-600 bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center">
         <h1 className="text-3xl font-bold primary-color ml-4">M.Z</h1>
@@ -24,15 +40,17 @@ export const Navbar = () => {
             <li className="p-5">
                 <Link href="/contact">Contact</Link>{" "}
             </li>
-            <li className="p-5">
+
+            {!loggedIn ? (<li className="p-5">
                 <Link href="/login">Login</Link>{" "}
-            </li>
-            <li className="p-5">
+            </li>) : (<li className="p-5">
                 <form action={logout}>
                     <button  type="submit" formAction={logout}>Logout</button>{" "}
                 </form>
                 
-            </li>
+            </li>)}
+            
+            
           
         </ul>
         
